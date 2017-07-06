6f79f10d98ace47349b0da8061feca355316c6df
Huy Vo <huyvx@nexlesoft.com>
2017-07-06 10:26:37 +0700

    create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,25 @@ import { HomeComponent } from './home/home.component';
 import { AppRoutes } from './app.routes';
 import { MenuComponent } from './menu/menu.component';
 import { SubMenuComponent } from './sub-menu/sub-menu.component';
+import { ContactComponent } from './contact/contact.component';
+import { RecruitmentComponent } from './recruitment/recruitment.component';
+import { NewsComponent } from './news/news.component';
+import { TipsComponent } from './tips/tips.component';
+import { EventsComponent } from './events/events.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     MenuComponent,
-    SubMenuComponent
+    SubMenuComponent,
+    ContactComponent,
+    RecruitmentComponent,
+    NewsComponent,
+    TipsComponent,
+    EventsComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,21 @@
 import { Routes, RouterModule } from '@angular/router';
 
 import {HomeComponent} from './home/home.component';
+import {ContactComponent} from './contact/contact.component';
+import {RecruitmentComponent} from './recruitment/recruitment.component';
+import {NewsComponent} from './news/news.component';
+import {ServicesComponent} from './services/services.component';
+import {EventsComponent} from './events/events.component';
+import {TipsComponent} from './tips/tips.component';
 
 const routing: Routes = [
   { path: '', component: HomeComponent} ,
-  { path: 'services', component: HomeComponent},
-  { path: 'events', component: HomeComponent},
-  { path: 'tips', component: HomeComponent},
-  { path: 'news', component: HomeComponent},
-  { path: 'recruitment', component: HomeComponent},
-  { path: 'contact', component: HomeComponent},
-  { path: 'contact', component: HomeComponent},
-  { path: 'contact', component: HomeComponent}
+  { path: 'services', component: ServicesComponent},
+  { path: 'events', component: EventsComponent},
+  { path: 'tips', component: TipsComponent},
+  { path: 'news', component: NewsComponent},
+  { path: 'recruitment', component: RecruitmentComponent},
+  { path: 'contact', component: ContactComponent}
 ]
 
 export const AppRoutes = RouterModule.forRoot(routing);
